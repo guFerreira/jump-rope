bd7f56caed74c51da41f727c45fe73c18a8486f5
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2023-01-24 08:55:31 -0300

    Initial commit

---
+++ src/main/java/com/gustavo/jumprope/JumpRopeApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.jumprope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpRopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpRopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/jumprope/JumpRopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.jumprope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpRopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
